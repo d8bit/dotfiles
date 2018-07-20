8aa26ed23ac6a560310f0fcc2b6e2181d379c04c
doliva <doliva@refineriaweb.com>
2018-07-20 16:14:56 +0200

    Added script to autoload tmux sessions

---
+++ tmuxScript.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+session="brokerBack"
+
+cd /var/www/html/brokercoin/backend/
+# set up tmux
+tmux start-server
+
+# create a new tmux session, starting vim from a saved session in the new window
+tmux new-session -d -s $session -n vim
+
+# open vim with session
+tmux send-keys "vim -S ~/.vim/sessions/brokerBack.vim" C-m
+
+session="brokerFront"
+
+cd /var/www/html/brokercoin/frontend/
+
+# create a new tmux session, starting vim from a saved session in the new window
+tmux new-session -d -s $session -n vim
+
+# create a new window called npm
+tmux new-window -t $session:2 -n npm
+
+# Split pane 1 vertical by 65%, start npm
+tmux splitw -v -p 35
+tmux send-keys "npm run watch" C-m
+
+# create a new window called TOR
+tmux new-window -t $session:3 -n TOR
+tmux send-keys "cd ~/test/tor/" C-m
+
+
+# return to main vim window
+tmux select-window -t $session:1
+
+# open vim with session
+tmux send-keys "vim -S ~/.vim/sessions/brokerFront.vim" C-m
+
+# Finished setup, attach to the tmux session!
+tmux attach-session -t $session
+
